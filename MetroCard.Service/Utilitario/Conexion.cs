56fad4d583ce1dba1011c2cd0cec051bd0280bb5
Alfredo Benaute <alfredo.benaute@hundred.com.pe>
2018-01-22 23:16:08 -0500

    1ra version

---
+++ MetroCard.Service/Utilitario/Conexion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace MetroCard.Service.Utilitario
+{
+    public class Conexion
+    {
+        public static string cnMetroCard = ConfigurationManager.ConnectionStrings["cnMetroCard"].ConnectionString;
+
+    }
+}
